2df5ee78e9ed15721c21d38ba06d438809b27fda
Pers test <qa61test@gmail.com>
2023-04-29 11:47:09 +0400

    initial commit

---
+++ src/main/java/ru/netology/JavaQAMaven/JavaDZ6_1/services/NumberMonthsRest.java
@@ -0,0 +1,19 @@
+package ru.netology.JavaQAMaven.JavaDZ6_1.services;
+
+public class NumberMonthsRest {
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0; // кол-во месяцев отдыха
+        int money = 0; // кол-во денег на счету
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) { // можем ли мы отдыхать?
+                count++;
+                money = money - expenses;
+                money = money / 3;
+            } else {
+                money=money + income - expenses;
+            }
+        }
+
+        return count;
+    }
+}
